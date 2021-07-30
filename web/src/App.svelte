3819e18116ff849c0cf33519f8f9ed7c8262cbad
iczc <iczcalan@gmail.com>
2021-07-30 18:20:42 +0800

    perf: delete global.css

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -5,8 +5,6 @@
 </script>
 
 <svelte:head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>ETH Testnet Faucet</title>
 </svelte:head>
 
