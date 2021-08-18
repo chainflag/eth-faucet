47dab1bad2785038925492e762de85eec964b055
slendermaan <421871384@qq.com>
2021-08-18 17:13:06 +0800

    new

--- web/src/Faucet.svelte
+++ web/src/Faucet.svelte
@@ -26,11 +26,6 @@
         removeAfter: 4000,
         position: 'bottom-center',
       });
-      window.scrollTo(
-        0,
-        document.documentElement.scrollHeight -
-          document.documentElement.clientHeight
-      );
       return;
     }
 
@@ -52,93 +47,115 @@
 </script>
 
 <main>
-  <body>
-    <section class="hero is-info is-fullheight">
-      <div class="hero-head">
-        <nav class="navbar">
-          <div class="container">
-            <div class="navbar-brand">
-              <a class="navbar-item" href="../">
-                <img src="./favicon.png" alt="Logo" />
-              </a>
-              <span class="navbar-burger burger" data-target="navbarMenu">
-                <span />
-                <span />
-                <span />
+  <section class="hero is-info is-fullheight">
+    <div class="hero-head">
+      <nav class="navbar">
+        <div class="container">
+          <div class="navbar-brand">
+            <a class="navbar-item" href="../">
+              <img src="./favicon.png" alt="Logo" />
+            </a>
+            <span class="navbar-burger burger" data-target="navbarMenu">
+              <span />
+              <span />
+              <span />
+            </span>
+          </div>
+          <div id="navbarMenu" class="navbar-menu">
+            <div class="navbar-end">
+              <span class="navbar-item">
+                <a class="button is-white is-outlined" href="#">
+                  <span class="icon">
+                    <i class="fa fa-home" />
+                  </span>
+                  <span>Home</span>
+                </a>
+              </span>
+              <span class="navbar-item">
+                <a class="button is-white is-outlined" href="#">
+                  <span class="icon">
+                    <i class="fa fa-superpowers" />
+                  </span>
+                  <span>Examples</span>
+                </a>
+              </span>
+              <span class="navbar-item">
+                <a class="button is-white is-outlined" href="#">
+                  <span class="icon">
+                    <i class="fa fa-book" />
+                  </span>
+                  <span>Documentation</span>
+                </a>
+              </span>
+              <span class="navbar-item">
+                <a
+                  class="button is-white is-outlined"
+                  href="https://github.com/chainflag/eth-faucet"
+                >
+                  <span class="icon">
+                    <i class="fa fa-github" />
+                  </span>
+                  <span>View Source</span>
+                </a>
               </span>
             </div>
-            <div id="navbarMenu" class="navbar-menu">
-              <div class="navbar-end">
-                <span class="navbar-item">
-                  <a class="button is-white is-outlined" href="#">
-                    <span class="icon">
-                      <i class="fa fa-home" />
-                    </span>
-                    <span>Home</span>
-                  </a>
-                </span>
-                <span class="navbar-item">
-                  <a class="button is-white is-outlined" href="#">
-                    <span class="icon">
-                      <i class="fa fa-superpowers" />
-                    </span>
-                    <span>Examples</span>
-                  </a>
-                </span>
-                <span class="navbar-item">
-                  <a class="button is-white is-outlined" href="#">
-                    <span class="icon">
-                      <i class="fa fa-book" />
-                    </span>
-                    <span>Documentation</span>
-                  </a>
-                </span>
-                <span class="navbar-item">
-                  <a
-                    class="button is-white is-outlined"
-                    href="https://github.com/chainflag/eth-faucet"
-                  >
-                    <span class="icon">
-                      <i class="fa fa-github" />
-                    </span>
-                    <span>View Source</span>
-                  </a>
-                </span>
-              </div>
-            </div>
           </div>
-        </nav>
-      </div>
-      <div class="hero-body">
-        <div class="container has-text-centered">
-          <div class="column is-6 is-offset-3">
-            <h1 class="title">
-              Receive {faucetInfo.payout} ETH per request
-            </h1>
-            <h2 class="subtitle">
-              $Serving from account {faucetInfo.account}
-            </h2>
-            <div class="box">
-              <div class="field is-grouped">
-                <p class="control is-expanded">
-                  <input
-                    bind:value={address}
-                    class="input"
-                    type="text"
-                    placeholder="Enter your address"
-                  />
-                </p>
-                <p class="control">
-                  <button on:click={handleRequest} class="button is-info"
-                    >Request</button
-                  >
-                </p>
-              </div>
+        </div>
+      </nav>
+    </div>
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div class="column is-6 is-offset-3">
+          <h1 class="title">
+            Receive {faucetInfo.payout} ETH per request
+          </h1>
+          <h2 class="subtitle">
+            $Serving from account {faucetInfo.account}
+          </h2>
+          <div class="box">
+            <div class="field is-grouped">
+              <p class="control is-expanded">
+                <input
+                  bind:value={address}
+                  class="input"
+                  type="text"
+                  placeholder="Enter your address"
+                />
+              </p>
+              <p class="control">
+                <button on:click={handleRequest} class="button is-info"
+                  >Request</button
+                >
+              </p>
             </div>
           </div>
         </div>
       </div>
-    </section>
-  </body>
-  <script async type="text/javascript" src="../build/bulma.js"></script>
+    </div>
+  </section>
 </main>
+
+<style>
+  html,
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+      Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+  }
+  .hero.is-info {
+    background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+      url('https://picsum.photos/1200/900') no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
+  .hero .nav,
+  .hero.is-success .nav {
+    -webkit-box-shadow: none;
+    box-shadow: none;
+  }
+  .hero .subtitle {
+    padding: 3rem 0;
+    line-height: 1.5;
+  }
+</style>
