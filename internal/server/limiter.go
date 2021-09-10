cd064715336185d08266d8f23879071e31566151
iczc <iczcalan@gmail.com>
2021-09-11 03:22:37 +0800

    feat: get client ip behind reverse proxy

--- internal/server/limiter.go
+++ internal/server/limiter.go
@@ -5,13 +5,36 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/ReneKroon/ttlcache/v2"
+	log "github.com/sirupsen/logrus"
 	"github.com/urfave/negroni"
 
 	"github.com/chainflag/eth-faucet/internal/chain"
 )
+
+var cidrs []*net.IPNet
+
+func init() {
+	maxCidrBlocks := []string{
+		"127.0.0.1/8",    // localhost
+		"10.0.0.0/8",     // 24-bit block
+		"172.16.0.0/12",  // 20-bit block
+		"192.168.0.0/16", // 16-bit block
+		"169.254.0.0/16", // link local address
+		"::1/128",        // localhost IPv6
+		"fc00::/7",       // unique local address IPv6
+		"fe80::/10",      // link local address IPv6
+	}
+
+	cidrs = make([]*net.IPNet, len(maxCidrBlocks))
+	for i, maxCidrBlock := range maxCidrBlocks {
+		_, cidr, _ := net.ParseCIDR(maxCidrBlock)
+		cidrs[i] = cidr
+	}
+}
 
 type Limiter struct {
 	cache *ttlcache.Cache
@@ -33,19 +56,19 @@ func (l *Limiter) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.Ha
 		http.Error(w, "invalid address", http.StatusBadRequest)
 		return
 	}
-	ip, err := getIP(r)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	if l.limitByKey(w, address) || l.limitByKey(w, ip) {
+	clintIP := getClientIPFromRequest(r)
+	if l.limitByKey(w, address) || l.limitByKey(w, clintIP) {
 		return
 	}
 
 	next.ServeHTTP(w, r)
 	if w.(negroni.ResponseWriter).Status() == http.StatusOK {
 		l.cache.SetWithTTL(address, true, l.ttl)
-		l.cache.SetWithTTL(ip, true, l.ttl)
+		l.cache.SetWithTTL(clintIP, true, l.ttl)
+		log.WithFields(log.Fields{
+			"address":  address,
+			"clientIP": clintIP,
+		}).Info("Maximum request limit has been reached")
 	}
 }
 
@@ -58,14 +81,39 @@ func (l *Limiter) limitByKey(w http.ResponseWriter, key string) bool {
 	return false
 }
 
-func getIP(r *http.Request) (string, error) {
-	ip, _, err := net.SplitHostPort(r.RemoteAddr)
-	if err != nil {
-		return "", err
-	}
-	if netIP := net.ParseIP(ip); netIP != nil {
-		return ip, nil
+// https://en.wikipedia.org/wiki/Private_network
+func isPrivateAddress(address string) (bool, error) {
+	ipAddress := net.ParseIP(address)
+	if ipAddress == nil {
+		return false, errors.New("address is not valid")
 	}
 
-	return "", errors.New("no valid ip found")
+	for i := range cidrs {
+		if cidrs[i].Contains(ipAddress) {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+func getClientIPFromRequest(r *http.Request) string {
+	remoteIP, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		remoteIP = r.RemoteAddr
+	}
+	isPrivate, err := isPrivateAddress(remoteIP)
+	if !isPrivate && err == nil {
+		return remoteIP
+	}
+
+	if xForwardedFor := r.Header.Get("X-Forwarded-For"); xForwardedFor != "" {
+		maxProxyCount := 1
+		xForwardedForList := strings.Split(xForwardedFor, ",")
+		return strings.TrimSpace(xForwardedForList[len(xForwardedForList)-maxProxyCount])
+	} else if xRealIP := r.Header.Get("X-Real-Ip"); xRealIP != "" {
+		return strings.TrimSpace(xRealIP)
+	}
+
+	return remoteIP
 }
