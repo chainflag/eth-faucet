e751d90fd5ee76e40eebdd36f3d23b26be06d5fc
MrxinL <64063895+MrxinL@users.noreply.github.com>
2023-04-29 23:31:36 +0800

    background-color update

--- web/src/Faucet.svelte
+++ web/src/Faucet.svelte
@@ -139,8 +139,7 @@
 
 <style>
   .hero.is-info {
-    background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-      url('/background.jpg') no-repeat center center fixed;
+    background: #000;
     -webkit-background-size: cover;
     -moz-background-size: cover;
     -o-background-size: cover;
