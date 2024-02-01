785916e42839ed714db99d5cb3a89563d0d85cab
Igor Goldobin <igor.goldobin@spendconsole.com>
2024-02-01 14:29:25 +1000

    fix: for env vars

--- cmd/server.go
+++ cmd/server.go
@@ -8,6 +8,7 @@ import (
 	"math/big"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 
 	"github.com/ethereum/go-ethereum/crypto"
@@ -20,42 +21,22 @@ var (
 	appVersion = "v1.1.0"
 	chainIDMap = map[string]int{"goerli": 5, "auroria": 205205, "sepolia": 11155111}
 
-	port :=  os.Getenv("HTTP_PLATFORM_PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	faucetAmount :=  os.Getenv("FAUCET_AMOUNT")
-	if faucetAmount == "" {
-		faucetAmount = 1
-	}
-
-	faucetSymbol :=  os.Getenv("FAUCET_SYMBOL")
-	if faucetSymbol == "" {
-		faucetSymbol = "tSTRAX"
-	}
-
-	faucetName :=  os.Getenv("FAUCET_NAME")
-	if faucetName == "" {
-		faucetName = "auroria"
-	}
-
-	httpPortFlag = flag.Int("httpport", port, "Listener port to serve HTTP connection")
+	//httpPortFlag = flag.Int("httpport", 8080, "Listener port to serve HTTP connection")
 	proxyCntFlag = flag.Int("proxycount", 0, "Count of reverse proxies in front of the server")
 	versionFlag  = flag.Bool("version", false, "Print version number")
 
-	payoutFlag   = flag.Int("faucet.amount", faucetAmount, "Number of Ethers to transfer per user request")
-	intervalFlag = flag.Int("faucet.minutes", 1440, "Number of minutes to wait between funding rounds")
-	netnameFlag  = flag.String("faucet.name", faucetName, "Network name to display on the frontend")
-	symbolFlag   = flag.String("faucet.symbol", faucetSymbol, "Token symbol to display on the frontend")
+	//payoutFlag   = flag.Int("faucet_amount", 10000, "Number of Ethers to transfer per user request")
+	intervalFlag = flag.Int("faucet_minutes", 1440, "Number of minutes to wait between funding rounds")
+	netnameFlag  = flag.String("faucet_name", os.Getenv("FAUCET_NAME"), "Network name to display on the frontend")
+	symbolFlag   = flag.String("faucet_symbol", os.Getenv("FAUCET_SYMBOL"), "Token symbol to display on the frontend")
 
-	keyJSONFlag  = flag.String("wallet.keyjson", os.Getenv("KEYSTORE"), "Keystore file to fund user requests with")
-	keyPassFlag  = flag.String("wallet.keypass", "password.txt", "Passphrase text file to decrypt keystore")
-	privKeyFlag  = flag.String("wallet.privkey", os.Getenv("PRIVATE_KEY"), "Private key hex to fund user requests with")
-	providerFlag = flag.String("wallet.provider", os.Getenv("WEB3_PROVIDER"), "Endpoint for Ethereum JSON-RPC connection")
+	keyJSONFlag  = flag.String("wallet_keyjson", os.Getenv("KEYSTORE"), "Keystore file to fund user requests with")
+	keyPassFlag  = flag.String("wallet_keypass", "password.txt", "Passphrase text file to decrypt keystore")
+	privKeyFlag  = flag.String("wallet_privkey", os.Getenv("PRIVATE_KEY"), "Private key hex to fund user requests with")
+	providerFlag = flag.String("wallet_provider", os.Getenv("WEB3_PROVIDER"), "Endpoint for Ethereum JSON-RPC connection")
 
-	hcaptchaSiteKeyFlag = flag.String("hcaptcha.sitekey", os.Getenv("HCAPTCHA_SITEKEY"), "hCaptcha sitekey")
-	hcaptchaSecretFlag  = flag.String("hcaptcha.secret", os.Getenv("HCAPTCHA_SECRET"), "hCaptcha secret")
+	hcaptchaSiteKeyFlag = flag.String("hcaptcha_sitekey", os.Getenv("HCAPTCHA_SITEKEY"), "hCaptcha sitekey")
+	hcaptchaSecretFlag  = flag.String("hcaptcha_secret", os.Getenv("HCAPTCHA_SECRET"), "hCaptcha secret")
 )
 
 func init() {
@@ -80,6 +61,19 @@ func Execute() {
 	if err != nil {
 		panic(fmt.Errorf("cannot connect to web3 provider: %w", err))
 	}
+
+	port, err := strconv.Atoi(os.Getenv("HTTP_PLATFORM_PORT"))
+	if err != nil {
+		port = 8080
+	}
+	httpPortFlag := flag.Int("httpport", port, "Listener port to serve HTTP connection")
+
+	faucetAmount, err := strconv.Atoi(os.Getenv("FAUCET_AMOUNT"))
+	if err != nil {
+		faucetAmount = 10000
+	}
+	payoutFlag := flag.Int("faucet.amount", faucetAmount, "Number of Ethers to transfer per user request")
+
 	config := server.NewConfig(*netnameFlag, *symbolFlag, *httpPortFlag, *intervalFlag, *payoutFlag, *proxyCntFlag, *hcaptchaSiteKeyFlag, *hcaptchaSecretFlag)
 	go server.NewServer(txBuilder, config).Run()
 
