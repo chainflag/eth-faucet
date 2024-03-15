a40346067dccff8f2eb4fac48b2cab1321ab08ab
Stellarthoughts <49097167+Stellarthoughts@users.noreply.github.com>
2024-03-15 16:48:40 +0700

    add: symbol env

--- web/src/env/env.ts
+++ web/src/env/env.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_LOGO_PATH: string;
   readonly VITE_BACKGROUND_PATH: string;
+  readonly VITE_FAVICON_PATH: string;
+  readonly VITE_SYMBOL: string;
 }
 
 interface ImportMeta {
