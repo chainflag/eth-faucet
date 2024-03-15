096d15e36549dbab3d00e0e725939f7bd4cd6584
Ivan Zubok <ivan@gateway.fm>
2024-03-15 17:54:25 +0200

    Support payout in Gwei Pass background and logo through command line

--- internal/server/dto.go
+++ internal/server/dto.go
@@ -26,6 +26,8 @@ type infoResponse struct {
 	Payout          string `json:"payout"`
 	Symbol          string `json:"symbol"`
 	HcaptchaSiteKey string `json:"hcaptcha_sitekey,omitempty"`
+	LogoUrl         string `json:"logo_url"`
+	BackgroundUrl   string `json:"background_url"`
 }
 
 type malformedRequest struct {
